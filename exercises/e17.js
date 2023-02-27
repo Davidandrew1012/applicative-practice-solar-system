717aeafd02e9c78c2c30493d3c017553314bda6c
david01relic <davidandrew1012@gmail.com>
2023-02-27 14:32:48 -0500

    e17 and e18 complete

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,3 +1,26 @@
-export function minBy(array, cb) {}
+export function minBy(array, cb) {
+    let lowest = undefined;
+    let lowestValue = undefined;
+    for (let i = 0; i < array.length; i++) {
+      const currentValue = cb(array[i]);
+      if (lowestValue === undefined || currentValue < lowestValue) {
+        lowest = array[i];
+        lowestValue = currentValue;
+      }
+    }
+    return lowest;
+  }
+  
+  export function maxBy(array, cb) {
+    let highest = undefined;
+    let highestValue = undefined;
+    for (let i = 0; i < array.length; i++) {
+      const currentValue = cb(array[i]);
+      if (highestValue === undefined || currentValue > highestValue) {
+        highest = array[i];
+        highestValue = currentValue;
+      }
+    }
+    return highest;
+  }
 
-export function maxBy(array, cb) {}
--- exercises/e18.js
+++ exercises/e18.js
@@ -5,11 +5,28 @@ import { data } from "../data/data";
 // Return example: 1902
 
 export function getGreatestDiscoveryYear(data) {
-  // Your code goes here...
-  // feel free to import your `maxBy` or `minBy` methods from previous lessons
+  const discoveryYears = data.asteroids.map(asteroid => asteroid.discoveryYear);
+  const discoveryYearCounts = discoveryYears.reduce((acc, year) => {
+    if (acc[year]) {
+      acc[year]++;
+    } else {
+      acc[year] = 1;
+    }
+    return acc;
+  }, {});
+  let greatestDiscoveryYear;
+  let highestCount = 0;
+  Object.keys(discoveryYearCounts).forEach(year => {
+    if (discoveryYearCounts[year] > highestCount) {
+      highestCount = discoveryYearCounts[year];
+      greatestDiscoveryYear = Number(year);
+    }
+  });
+  return greatestDiscoveryYear;
 }
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-16"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
+
